bcdb2689f520434b0d4f9db882df2574385cb6c7
flagnarb <jinx.dakota@gmail.com>
2025-02-01 10:37:11 -0500

    ADDED RUBIES

--- fabric-tutorial-1.20.x/src/main/java/net/joules/tutorialmod/TutorialMod.java
+++ fabric-tutorial-1.20.x/src/main/java/net/joules/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.joules.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.joules.tutorialmod.item.ModItemGroups;
+import net.joules.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 
 	}
 }
